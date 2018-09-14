0f65920ceff4904f2253839f360c28df0b5cadbe
Alfons Flystam <alfons.flystam@hotmail.com>
2018-09-14 14:00:52 +0200

    Hej

---
+++ Game1/Game1/Bas.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Bas
+    {
+        protected Texture2D tex;
+        protected Vector2 pos;
+        protected int height;
+        protected int width;
+
+        public virtual void Update() { }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tex,new Rectangle(pos.ToPoint(),new Point(width, height)), Color.BlanchedAlmond);
+        }
+
+        public Texture2D Tex
+        {
+            get { return tex; }
+            set { tex = value; }
+        }
+
+        public Vector2 Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+        }
+    }
+}
---
+++ Game1/Game1/Varelser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Varelser:Bas
+    {
+        protected Rectangle hitBox;
+        protected int hP;
+        protected double speed;
+
+        public Rectangle HitBox
+        {
+            get { return hitBox; }
+            set { hitBox = value; }
+        }
+
+        public int HP
+        {
+            get { return hP; }
+            set { hP = value; }
+        }
+
+        public double Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+    }
+}
